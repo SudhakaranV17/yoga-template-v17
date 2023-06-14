4e758fa57001f53edc967db70c30cdd231156720
SudhakaranV17 <sudhakaranv17@gmail.com>
2023-06-14 23:22:41 +0530

    finished upto events section

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "12.5rem";
+  }
+  
+function closeNav() {
+   document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+// aos animation 
+AOS.init();
